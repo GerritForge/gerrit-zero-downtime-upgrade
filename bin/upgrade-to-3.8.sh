42acdef8ce75c213e6175b551ff752d1117f5305
Luca Milanesio <luca.milanesio@gmail.com>
2023-09-26 22:22:06 +0100

    Online upgrade to Gerrit v3.8

--- bin/upgrade-to-3.8.sh
+++ bin/upgrade-to-3.8.sh
@@ -6,7 +6,7 @@ function title {
   echo $(echo $1 | sed 's/./=/g')
 }
 
-title "Build Gerrit v3.7 image"
+title "Build Gerrit v3.8 image"
 docker-compose build
 
 title "Make gerrit-01 primary unhealthy"
@@ -15,9 +15,7 @@ sleep 20
 
 title "Kill gerrit-01 primary"
 docker-compose kill gerrit-01
-
-title "Upgrade gerrit-01 primary"
-docker-compose up gerrit-01-upgrade
+docker-compose rm -f gerrit-01
 
 title "Start gerrit-01 primary"
 docker-compose up -d gerrit-01
@@ -25,11 +23,15 @@ docker-compose up -d gerrit-01
 title "Wait for gerrit-01 to become healthy again"
 while ! curl -s -f http://localhost:8081/config/server/healthcheck\~status; do echo -n '.' && sleep 1; done
 
+echo "Has the upgrade been successful ?"
+read upgradeOk
+
 title "Make gerrit-02 primary unhealthy"
 ssh -p 29412 admin@localhost gerrit plugin rm healthcheck
 sleep 20
 
 title "Restart gerrit-02 primary"
 docker-compose kill gerrit-02
+docker-compose rm -f gerrit-02
 docker-compose up -d gerrit-02
 
